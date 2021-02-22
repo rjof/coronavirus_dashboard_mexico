fb5bd055b3d8eb8dac558bdd5238c6e821fe942d
Ricardo Jose Olvera Flores <ricardo.olvera.e@thalesdigital.io>
2021-02-22 10:01:46 -0600

    Plots of Mexico place by metric.

--- toPutInDashboard.R
+++ toPutInDashboard.R
@@ -165,11 +165,79 @@ ggplot(data = a1, aes(x = date)) +
 #' 
 #' How to use this:
 #' "apple_mtr_driving"      "apple_mtr_walking"     
-#' [22] "apple_mtr_transit"      "gcmr_retail_recreation" "gcmr_grocery_pharmacy" 
-#' [25] "gcmr_parks"             "gcmr_transit_stations"  "gcmr_workplaces"       
-#' [28] "gcmr_residential"       "gtrends_score"          "gtrends_country_score" 
+#' "apple_mtr_transit"      "gcmr_retail_recreation" "gcmr_grocery_pharmacy" 
+#' "gcmr_parks"             "gcmr_transit_stations"  "gcmr_workplaces"       
+#' "gcmr_residential"       "gtrends_score"          "gtrends_country_score" 
 #' 
 #' For each variable v1 get
 #' i1 = v1 by date by country * 100000 / population
 #' Show top 3, bottom 3, Mexico, 3 above Mexico, 3 below Mexico
 #' and some distance indicator
+
+m2mex = merged[merged$iso3c=="MEX",]
+n = nrow(m2mex)
+theNames = names(m2mex)
+x3 = sapply(1:ncol(m2mex), function(c1){
+  ifelse(test = n==sum(is.na(m2mex[,theNames[c1]]))
+         , yes = print(paste(theNames[c1],"is empty"))
+         ,  no = print(paste(theNames[c1],"has", sum(!is.na(m2mex[,theNames[c1]])),"not empty rows from", n)))
+})
+
+## Only for max date
+m2 = merged %>%
+  select(date, iso3c, country, population,
+         confirmed, ecdc_cases,
+         deaths, ecdc_deaths,
+         recovered,
+         total_tests,
+         positive_rate,
+         #hosp_patients,
+         #icu_patients,
+         total_vaccinations
+  ) %>%
+  filter(date == max(date)) %>%
+  mutate(
+    confirmedX100K = confirmed * 100000 / population,
+    ecdc_casesX100K = ecdc_cases * 100000 / population,
+    deathsX100K = deaths * 100000 / population,
+    ecdc_deathsX100K = ecdc_deaths * 100000 / population,
+    recoveredX100K = recovered * 100000 / population,
+    total_testsX100K = total_tests * 100000 / population,
+    positive_rateX100K = positive_rate * 100000 / population,
+    #hosp_patientsX100K = hosp_patients * 100000 / population,
+    #icu_patientsX100K = icu_patients * 100000 / population,
+    total_vaccinationsX100K = total_vaccinations * 100000 / population
+  ) %>%
+  select(iso3c,country,confirmedX100K, 
+         ecdc_casesX100K, deathsX100K, 
+         ecdc_deathsX100K, recoveredX100K, 
+         total_testsX100K, positive_rateX100K, 
+         total_vaccinationsX100K)
+
+## Mexican place for each metric
+x3 = sapply(names(m2), function(thisMetric){
+  c1 = m2[,c("iso3c","country",thisMetric)]
+  c1 = c1[!is.na(c1[,thisMetric]),]
+  if(nrow(c1) > 0 && nrow(c1[c1$iso3c=="MEX",thisMetric])==1) {
+    c1 = c1[order(c1[,thisMetric]),]
+    c1$place = seq(1:nrow(c1))
+    mex.place = which(c1$iso3c=="MEX")
+    print(paste("Mexico place:", mex.place,"for",thisMetric,"from",nrow(c1)))
+    c1
+  }
+})
+x3[sapply(x3, is.null)] <- NULL
+
+## testing plots
+tv=x3[["total_vaccinationsX100K"]]
+tv=x3[["deathsX100K"]]
+highlight=which(tv$iso3c=="MEX")
+#tv[,"iso3c"] <- as.factor(tv[,"iso3c"])
+bar.colors = c(rep(x="blue",highlight-1),"red",rep(x="blue",nrow(tv)-highlight))
+ggplot(data = tv) +
+  geom_bar(aes(x = reorder(country, place), y = tv[[3]], fill = as.factor(place)), 
+           stat = "identity") +
+  theme(axis.text.x = element_text(angle = 45),
+        legend.position = "none") +
+  scale_fill_manual(values = bar.colors)
+
